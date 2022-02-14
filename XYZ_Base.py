08b28393b1489bb2750ec9eff95d10039a97cd81
mannergam91 <57859846+mannergam91@users.noreply.github.com>
2022-02-14 13:57:17 +0300

    Update XYZ_Base.py

--- XYZ_Base.py
+++ XYZ_Base.py
@@ -2,11 +2,11 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+
 class BasePage:
 
     def __init__(self, driver):
         self.driver = driver
-
 
     def find_element_located(self, link, timeout=5):
         WbDrW = WebDriverWait(self.driver, timeout)
@@ -15,13 +15,14 @@ class BasePage:
     def go_to_site(self):
         return self.driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login")
 
-    def sum_string_drop_down (self, line):
+    def sum_string(self, line):
         time.sleep(1)
         strings = self.driver.find_elements_by_xpath(line)
         sum_str = len(strings)
         return sum_str
 
-    def names_list_drop_down (self, sum_str, line):
+    def names_list(self, sum_str, line):
+        time.sleep(1)
         lst_names = list()
         x = 0
         while x < (sum_str - 1):
@@ -31,9 +32,22 @@ class BasePage:
             lst_names.extend([scan_name])
         return lst_names
 
-    def serch_namber_name (self, lst_names, name):
-        x = 0
-        while lst_names[x] != name:
-            x += 1
-        nambe_name = x + 1
-        return nambe_name
+    def serch_namber_name(self, lst_names, name):
+        i = lst_names.index(name)
+        return i + 1
+
+    # def serch_namber_name(self, lst_names, name):
+    #     x = 0
+    #     while lst_names[x] != name:
+    #         x += 1
+    #     return x + 1
+
+    def names_cycle_text(self, line):
+        for element in self.driver.find_elements_by_xpath(line):
+            return element.text
+
+    def names_list_cycle_text(self, line):
+        lst = list()
+        for element in self.driver.find_elements_by_xpath(line):
+            lst.append(element.text)
+        return lst
